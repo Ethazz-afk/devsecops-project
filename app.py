7e6b1e8291088946c59d15903939ab2b6b723fc2
Ethazz <omar.hammami@etu.u-pec.fr>
2026-09-17 11:53:31 +0200

    modification app

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
     return 'Hello World'
 
-@app.route('/health')
+@app.route('/health', methods=['GET'])
 def health_check():
     return 'Health check passed'
 
